e3ed1efec6cc67547d382e27a6da15000fd5b0b0
RyanPagcatipunan <ryan.pagcatipunan@obf.ateneo.edu>
2021-07-10 19:18:14 +0800

    Update FirstmarkdownAssignment.R

--- FirstMarkdownAssignment/FirstmarkdownAssignment.R
+++ FirstMarkdownAssignment/FirstmarkdownAssignment.R
@@ -1,18 +1,18 @@
 print ("Dizon, Jannuel Lauro T.")
-<p>&nbsp;</p>
+
 print ("1. The reason we need to study and analyze data")
-<p>&nbsp;</p>
+
 print ("2. One application of data science in the day to day life would be Virtual Assitant with the likes of Google, Siri, Alexa, Bixby, and many others. Virtual Assistant uses voice commands to perform task requested by the user like what is weather today or what are some recipe for a cake.")
-<p>&nbsp;</p>
+
 [link](https://towardsdatascience.com/10-awesome-real-world-applications-of-data-science-and-ai-6fddf3f34d74)
-<p>&nbsp;</p>
+
 print ("3. A data science topic that I would like to pursue would be finding out what causes traffic, in terms of road design, here in the NCR and the surrounding regions. The data I would like to use would be coming from apps like Google maps and Waze that show us where there are traffic and how to avoid them. Knowing where the traffic start  we can then check that place and analyse what could be the underlying issue would be, rather than relying on road widening. The Statistical method that will be utilized here would be Prescriptive analysis. Anyone who travels could benefit from this because of the reduced travel time if it will be implemented properly.")
-<p>&nbsp;</p>
+
 print ("Pagcatipunan, Ryan P.")
-<p>&nbsp;</p>
+
 print ("1. One reason why we need to study data is the fact that we can understand how patterns emerge  from data about our lives and use these patterns to our advantage")
-<p>&nbsp;</p>
+
 print ("2.Data science could be applied in the field of finances. It could help make decisions such as stock market prediction and it could also show a financial report for individuals")
 [link](https://towardsdatascience.com/10-awesome-real-world-applications-of-data-science-and-ai-6fddf3f34d74)
-<p>&nbsp;</p>
+
 print ("3. I believe it would be interesting if we could apply data science to arts like artworks and music. This could be useful to companies that would like to cater to younger audiences. Data science could be used to understand the patterns of trending art and how these trends form. This could also be useful to art professionals and musicians who would want to understand why art and music differs from traditional art and music. Culture studies could benefit from data science because it gives another way to show how our culture of art and music is evolving from previous forms")
